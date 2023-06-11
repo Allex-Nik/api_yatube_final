99e082a840555110a8c42c24f84c820852e8c7d9
Allex-Nik <alexandr.nikolaev96@gmail.com>
2023-06-11 23:14:40 +0400

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,34 @@
+from rest_framework import viewsets
+from posts.models import Post, Group, Comment
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permission import IsAuthorOrReadOnly
+from rest_framework.exceptions import MethodNotAllowed
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly, IsAuthenticated)
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_id']
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        serializer.save(author=self.request.user, post_id=post_id)
